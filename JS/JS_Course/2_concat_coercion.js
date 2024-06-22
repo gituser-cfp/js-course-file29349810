f96832a4790ac7eda5513c6d9ff24395dc2e0c48
gituser-cfp <abhishekkumar200u@gmail.com>
2024-06-22 21:10:39 +0530

    Add files via upload

---
+++ JS/JS_Course/2_concat_coercion.js
@@ -0,0 +1,33 @@
+//* ============================
+//* Data Types Section - part 2
+//* ============================
+
+//* Concatenation in JavaScript
+//? In JavaScript, the + sign is not only used for arithmetic addition but also for string concatenation. When the + operator is used with strings, it concatenates the strings together.
+//? It's important to note that if any operand of the + operator is a string, JavaScript will treat the other operands as strings as well, resulting in string concatenation. If both operands are numbers, the + operator performs numeric addition.
+
+// const str = "Hello " + "World";
+// console.log(str);
+
+//* Type coercion is the automatic conversion of "values" from one data type to another.
+//? It is a fundamental part of JavaScript and can be used to make code more readable and efficient.
+//? There are two types of coercion in JavaScript: implicit and explicit. Implicit coercion happens automatically, while explicit coercion is done manually by the programmer.
+//! It's worth noting that type coercion can lead to unexpected results, so it's essential to be aware of how JavaScript handles these situations.
+
+// let sum = "5" + 10;
+// console.log(sum);
+
+//* ============================
+//* Tricky Interview Questions
+//* ============================
+console.log(10 + "20");                 //?Output = ~ 1020 ~           
+console.log(9 - "5");                   //?Output = ~ 4 ~             
+console.log("Java" + "Script");         //?Output = ~ JavaScript ~
+console.log(" " + " ");                 //?Output = ~ ~
+let sum = " " + 0;                      
+console.log(typeof sum);                //?Output = ~ String ~
+console.log("vinod" - "thapa");         //?Output = ~ NaN ~
+console.log(true + true);               //?Output = ~ 2 ~
+console.log(true + false);              //?Output = ~ 1 ~
+console.log(false + true);              //?Output = ~ 1 ~
+console.log(false - true);              //?Output = ~ -1 ~
---
+++ JS/JS_Projects/Left-out-Interview-Questions.js
@@ -0,0 +1,50 @@
+//! Reverse a String:
+//! Write a function to reverse a given string without using built-in reverse methods.
+
+// const isReverse = (str) => {
+//   let reverse = "";
+//   for (let char = str.length - 1; char >= 0; char--) {
+//     reverse = reverse + str[char];
+//   }
+//   return reverse;
+// };
+
+// console.log(isReverse("vinod thapa"));
+
+//! Palindrome Check:
+//! Create a function to determine if a given string is a palindrome (reads the same backward as forward).
+
+
+// // isi     isi
+// radar
+// level
+// const isPalindrome = (str) => {
+//   let reverse = "";
+//   for (let char = str.length - 1; char >= 0; char--) {
+//     reverse = reverse + str[char];
+//   }
+//   //   if (str === reverse) {
+//   //     return true;
+//   //   } else {
+//   //     return false;
+//   //   }
+
+//   return str === reverse ? true : false;
+// };
+
+// console.log(isPalindrome("level"));
+
+
+// //! Filter unique values
+// const n = [1, 2, 3, 4, 6, 5, 6, 7, 8, 9];
+
+// let uniqueValues = numbers.filter((curElem, index, arr) => {
+//   //   console.log(index);
+//   //   console.log(arr.indexOf(curElem));
+//   return arr.indexOf(curElem) === index;
+// });
+// console.log(uniqueValues);
+// console.log([...new Set(numbers)]);
+
+
+
